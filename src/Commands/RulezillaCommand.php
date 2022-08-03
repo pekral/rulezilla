bfd9bf04c87d89c59d7e30a7533b64ea6dedf9a9
Petr Král <kral.petr.88@gmail.com>
2022-08-03 15:47:41 +0200

    Commands - better console UI

--- src/Commands/RulezillaCommand.php
+++ src/Commands/RulezillaCommand.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace Rulezilla\Commands;
 
+use SebastianBergmann\Timer\Duration;
+use SebastianBergmann\Timer\Timer;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -11,6 +13,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 use function assert;
 use function mb_strtolower;
+use function sprintf;
 use function ucfirst;
 
 use const PHP_EOL;
@@ -19,6 +22,8 @@ abstract class RulezillaCommand extends Command
 {
 
     protected static string $rootDir;
+
+    private Timer $timer;
 
     abstract protected function getProcessCommand(): string;
 
@@ -29,28 +34,19 @@ abstract class RulezillaCommand extends Command
         parent::__construct();
 
         self::$rootDir = $config['rulezilla']['rootDir'];
+        $this->timer = new Timer();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        assert($input->isInteractive());
+        $isFixer = $this instanceof Fixer;
         $isDebugMode = $input->getOption('debug') !== null;
 
-        if ($isDebugMode) {
-            $output->writeln($this->getProcessCommand());
-        }
-
+        $this->printHeader($output, $isFixer);
+        $this->timer->start();
         exec($this->getProcessCommand(), $cliOutput, $resultCode);
-
-        assert($input->isInteractive());
-
-        if ($resultCode !== Command::SUCCESS) {
-            $output->writeln(implode(PHP_EOL, $cliOutput));
-        } elseif ($this instanceof Fixer) {
-            $output->writeln(sprintf('<info>%s | fix</info>', ucfirst(mb_strtolower($this->getConfigKey()))));
-            $output->writeln(implode(PHP_EOL, $cliOutput));
-        } else {
-            $output->writeln(sprintf('<info>✅ %s</info>', ucfirst(mb_strtolower($this->getConfigKey()))));
-        }
+        $this->printResult($output, $resultCode, $cliOutput, $isDebugMode, $this->timer->stop(), $isFixer);
 
         return $resultCode;
     }
@@ -73,6 +69,35 @@ abstract class RulezillaCommand extends Command
     protected function configure(): void
     {
         $this->addOption('debug', 'd', InputOption::VALUE_OPTIONAL, 'Show debug info');
+    }
+
+    private function printResult(OutputInterface $output, int $resultCode, array $cliOutput, bool $isDebugMode, Duration $duration, bool $isFixer): void
+    {
+        if ($resultCode !== Command::SUCCESS && !$isFixer) {
+            $output->write('<error>[ERROR]</error>');
+            $output->writeln(implode(PHP_EOL, $cliOutput));
+
+        } else {
+            $output->write('<info>[OK]</info>', true);
+        }
+
+        if (!$isDebugMode) {
+            return;
+        }
+
+        $output->writeln('<info>[DEBUG]</info>');
+        $output->writeln('[Command] '. $this->getProcessCommand());
+        $output->writeln('[Executing time] '. $duration->asString());
+    }
+
+    private function printHeader(OutputInterface $output, bool $isFixer): void
+    {
+        $commandConfigKey = ucfirst(mb_strtolower($this->getConfigKey()));
+        $output->write(sprintf('<comment>[%s]</comment>', $commandConfigKey));
+
+        $processInfo = $isFixer ? ' fixing errors ' : ' checking code ';
+
+        $output->write($processInfo);
     }
 
 }
