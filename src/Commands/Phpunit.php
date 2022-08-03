7fed57e5d5a0e750c87cd7a9334f582f60fbc790
Petr Král <pekral@users.noreply.github.com>
2022-08-03 12:28:22 +0200

    Phing - support PHPUnit now (#8)
    * Phing - support PHPUnit now
    
    * Phpunit - new command
    
    * PHPLint - new command
    
    * Phing - remove
    
    * Lint - more command options
    
    * PHPLint - Github actions update

---
+++ src/Commands/Phpunit.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Rulezilla\Commands;
+
+final class Phpunit extends RulezillaCommand
+{
+
+    /**
+     * @var string|null
+     */
+    protected static $defaultName = 'phpunit:run';
+
+    /**
+     * @var string|null
+     */
+    protected static $defaultDescription = 'Check PHP tests';
+
+    protected function getProcessCommand(): string
+    {
+        $targets = $this->getTargets();
+
+        $commandParts = [
+            sprintf(
+                'php %s/vendor/bin/phpunit %s --dont-report-useless-tests --strict-coverage --default-time-limit 0.75 --enforce-time-limit --colors auto',
+                self::$rootDir,
+                implode(' ', $targets),
+            ),
+        ];
+
+        if (isset($this->getConfig()['config'])) {
+            $commandParts[] = sprintf('-c %s/%s', self::$rootDir, $this->getConfig()['config']);
+        }
+
+        return implode(' ', $commandParts);
+    }
+
+    protected function configure(): void
+    {
+        $this->setHelp('Check PHP tests');
+    }
+
+    protected function getConfigKey(): string
+    {
+        return 'phpunit';
+    }
+
+}
